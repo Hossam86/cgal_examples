75fbd4350f2c60055736ad3decfd8e92ecb6d8ba
Hossam Samir <eng.hsamir@gmail.com>
2019-09-02 17:55:32 +0200

    inheritance of surface_mesh

---
+++ SurfaceMesh/sm_derivation.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<CGAL/Simple_cartesian.h>
+#include<CGAL/Surface_mesh.h>
+#include<CGAL/Polygon_mesh_processing/bbox.h>
+
+typedef CGAL::Simple_cartesian<double> K;
+typedef K::Point_3 Point;
+
+namespace My {
+struct Mesh:public CGAL::Surface_mesh<Point>{
+    typedef CGAL::Surface_mesh<Point> Base;
+    std::string name;
+
+};
+} //namespace My
+
+
+namespace boost {
+template <>
+struct graph_traits<My::Mesh>:public boost::graph_traits<My::Mesh::Base>
+{};
+
+template <typename T>
+struct property_map<My::Mesh,T>:public boost::property_map<My::Mesh::Base,T>
+{};
+}
+
+namespace CGAL {
+template <typename T>
+struct graph_has_property<My::Mesh,T>:public CGAL::graph_has_property<My::Mesh::Base,T>
+{};
+}
+
+
+int main()
+{
+    My::Mesh mesh;
+    CGAL::make_triangle(Point(0,0,0), Point(1,0,0), Point(1,1,1), mesh);
+    typedef boost::graph_traits<My::Mesh>::vertex_descriptor vertex_descriptor;
+
+    typedef boost::property_map<My::Mesh,CGAL::vertex_point_t>::type Point_property_map;
+    Point_property_map ppm=get(CGAL::vertex_point,mesh);
+
+    BOOST_FOREACH(vertex_descriptor vd , vertices(mesh))
+    {
+        if(vd !=boost::graph_traits<My::Mesh>::null_vertex())
+        {
+            std::cout<<vd << " at " <<get(ppm,vd) <<std::endl;
+        }
+    }
+    std::cout<<CGAL::Polygon_mesh_processing::bbox(mesh) <<std::endl;
+}
